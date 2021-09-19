6951505aec82cf25eddb029e12b1c0009ac2bacd
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2021-09-19 07:11:22 -0700

    Kick Command [en-us] locale support.

--- source/Localisation/en.js
+++ source/Localisation/en.js
@@ -1,8 +1,26 @@
 module.exports = {
+    "$cmd_usage": "I'm sorry but that command requires other paramaters...\n  Please try ",
+
+
     "$cmd_ping_Pinging": "Pinging...",
     "$cmd_ping_Pong": "Pong!",
     "$cmd_ping_Client": "Client",
-    "$cmd_ping_API": "API"
+    "$cmd_ping_API": "API",
 
 
+    "$cmd_kick_name": "kick",
+    "$cmd_kick_usage": "<@user|userID> [reason]",
+
+    "$cmd_kick_kick-succ": "Alright I've kicked the pest.",
+    "$cmd_kick_kick-fail": "I couldn't kick them!!\nRight click on this mentioned name and select `Kick Member`!\n-->",
+
+    "$cmd_kick_ban-succ": "Alright, I've banned ",
+    "$cmd_kick_ban-fail": "I couldn't ban them!!\nRight click on this mentioned name and select `Ban Member`!\n-->",
+    "$cmd_kick_ban-no": "❎\n  Uhm... Okay then...",
+
+    "$cmd_kick_noMember": "Please mention a user or give me their ID.",
+    "$cmd_kick_noMemberFound": "isn't on this server...",
+    "$cmd_kick_noMemberFound-ban": "isn't on this server...\nDid you mean to ban them?",
+    "$cmd_kick_noPerms-Author": "What are you trying to do??\nI will not kick someone for you...",
+    "$cmd_kick_noPerms-Client": "I cannot kick people on this server!\nRight click on this mentioned name and select the red `Kick ",
 }

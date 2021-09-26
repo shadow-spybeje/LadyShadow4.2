53ab44cdc95c43ca6c6cc1f86756d5d9de5c1ed2
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2021-09-26 00:45:20 -0700

    Logger now writes to .txt files as well as the console.

--- source/other/Utilities/logger.js
+++ source/other/Utilities/logger.js
@@ -1,8 +1,54 @@
 const Logger = {};
 module.exports = Logger;
 
+
+const timeStamp = async function(){
+    var result="";
+    var d = new Date();
+
+    let year = d.getFullYear();
+    let month = d.getMonth()+1; if(month.toString().length == 1) month = `0${month}`;
+    let day = d.getDate(); if(day.toString().length == 1) day = `0${day}`;
+    let hour = d.getHours(); if(hour.toString().length == 1) hour = `0${hour}`;
+    let minute = d.getMinutes(); if(minute.toString().length == 1) minute = `0${minute}`;
+    let seconds = d.getSeconds(); if(seconds.toString().length == 1) seconds = `0${seconds}`;
+    let milSeconds = d.getMilliseconds();
+    if(milSeconds.toString().length == 1){ milSeconds = `00${"milSeconds"}`; }
+    else if(milSeconds.toString().length == 2){ milSeconds = `0${milSeconds}`; };
+
+    result += `${year}/${month}/${day} ${hour}:${minute}:${seconds} (${milSeconds})`
+    return result;
+};
+
 const log = function(txt){
     console.log(`\n${txt}`);
+};
+
+const fs = require('fs')
+
+const validFiles = ['err', 'warn', 'log', 'debug'];
+const filePrint = async function(data){
+
+    if(data.file && !validFiles.includes(data.file)) return `${data.file} is not a supported file... Try these instead: ${validFiles.join(", ")}`;
+
+    let timestamp = await timeStamp();
+    let fileContent = `\n\n[${timestamp}]\n> ${data.msg}`;
+
+    if(data.file) fs.writeFile(`./source/logs/${data.file}.txt`, fileContent, { flag: 'a+' }, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+    });
+
+    fs.writeFile(`./source/logs/log.txt`, fileContent, { flag: 'a+' }, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+    });
 };
 
 /**
@@ -11,6 +57,8 @@ const log = function(txt){
  */
 Logger.emit = function(msg){
     console.log(`> ${msg}`);
+
+    filePrint({msg:msg});
 };
 
 /**
@@ -20,6 +68,7 @@ Logger.emit = function(msg){
 Logger.print = function(msg){
     if(!msg) return;
     log(`> ${msg}`);
+    filePrint({msg:msg});
 };
 
 /**
@@ -29,6 +78,7 @@ Logger.print = function(msg){
 Logger.debug = function(msg){
     if(!msg) return;
     log(`(DEBUG) ${msg}`);
+    filePrint({file: 'debug', msg:`(DEBUG) ${msg}\n${error}`});
 };
 
 /**
@@ -38,6 +88,7 @@ Logger.debug = function(msg){
 Logger.warn = function(msg){
     if(!msg) return;
     log(`(WARNING) ${msg}`);
+    filePrint({file: 'warn', msg:`(WARNING) ${msg}\n${error}`});
 };
 
 /**
@@ -47,5 +98,8 @@ Logger.warn = function(msg){
 Logger.error = function(msg, error){
     if(!msg) return;
     log(`(ERROR) ${msg}`);
-    if(error) console.error(error);
+    if(error){
+        console.error(error);
+        filePrint({file: 'err', msg:`(ERROR) ${msg}\n${error.stack}`});
+    };
 };

069d747d0f896112e6eaacd57b3dbce61a846769
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2021-09-26 00:45:45 -0700

    Ban command with locale-en

---
+++ source/Commands/Moderation/ban.js
@@ -0,0 +1,64 @@
+const L = async function(m,key){
+    return await m.client.l(m,`$cmd_ban_${key}`);
+};
+
+command = {
+    name: "ban",
+    name_L: "$cmd_ban_name",
+    aliases: [],
+    description: "$cmd_ban_desc",
+    usage: "$cmd_ban_usage",
+    args: true,
+
+    Access: null,
+    EasterEgg: true,
+};
+
+command.help = "";
+
+command.Execute = async function(message, args){
+    if(message.author.id != "213250789823610880")return
+
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply(await L(message, "noPerms-Author"));
+    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply(`${await L(message, `noPerms-Client`)} ${member.username}\`!\n--> ${member}`)
+
+    let member = await message.client.util.helpers.getUserMention(args.shift());
+    if(!member) return message.reply(await L(message, "noMember"));
+    //if(!member.bannable) return message.reply(await L(message, "notBannable"));
+
+    let reason = "";
+    if(args.length == 0){
+        reason = await L(message, 'reason-x');
+        reason = reason.replace('{author}', message.author.tag).replace('{authorID}', message.author.id);
+    }else{
+        reason = await L(message, 'reason');
+        reason = reason.replace('{author}', message.author.tag).replace('{authorID}', message.author.id).replace('{reason}', args.join(" "));
+    };
+
+
+    message.guild.members.ban(member, {reason: reason})
+    .then(async () => {
+        message.react('✅');
+        let txt = "";
+
+        "Alright, I've banned {user}\nReason: {reason}"
+        if(args.length > 0){ //notice with reason
+            txt = await L(message, 'ban-succ-r');
+            txt = txt.replace('{@user}', member).replace('{reason}', args.join(" "));
+        }else{ //notice without reason
+            txt = await L(message, 'ban-succ');
+            txt = txt.replace('{@user}', member);
+        };
+
+        message.channel.send(txt);
+    })
+    .catch(async (err) => {
+        let errR = await L(message, 'ban-fail');
+        await errR.replace('{@user}', member.tag).replace('{err}', `\`${err.name}: ${err.message}\``);
+        message.channel.send(errR);
+        message.client.util.logger.error(err, err);
+    });
+
+};
+
+module.exports = command;
--- source/Localisation/en.js
+++ source/Localisation/en.js
@@ -2,13 +2,22 @@ module.exports = {
     "$cmd_usage": "I'm sorry but that command requires other paramaters...\n  Please try ",
 
 
+    "$cmd_ping_name": "ping",
+    "$cmd_ping_desc": "",
+
     "$cmd_ping_Pinging": "Pinging...",
     "$cmd_ping_Pong": "Pong!",
     "$cmd_ping_Client": "Client",
     "$cmd_ping_API": "API",
 
 
+    // ====== ==== ======\\
+    // === MODERATION ===\\
+    // ====== ==== ======\\
+
+    // ====== KICK ====== \\
     "$cmd_kick_name": "kick",
+    "$cmd_kick_desc": "",
     "$cmd_kick_usage": "<@user|userID> [reason]",
 
     "$cmd_kick_kick-succ": "Alright I've kicked the pest.",
@@ -23,4 +32,22 @@ module.exports = {
     "$cmd_kick_noMemberFound-ban": "isn't on this server...\nDid you mean to ban them?",
     "$cmd_kick_noPerms-Author": "What are you trying to do??\nI will not kick someone for you...",
     "$cmd_kick_noPerms-Client": "I cannot kick people on this server!\nRight click on this mentioned name and select the red `Kick ",//{username}
+
+
+    // ====== BAN ====== \\
+
+    "$cmd_ban_name": "ban",
+    "$cmd_ban_desc": "",
+    "$cmd_ban_usage": "<@user|userID> [reason]",
+
+
+    "$cmd_ban_noMember": "Please mention a user or give me their ID.",
+    "$cmd_ban_notBannable": "I cannot ban this user...\n  They are either the Owner or have a role equal or greater than my highest..",
+    "$cmd_ban_noPerms-Author": "What are you trying to do??\nI will not ban someone for you...",
+    "$cmd_ban_noPerms-Client": "I cannot ban people on this server!\nRight click on this mentioned name and select the red `Ban ",//{username}
+    "$cmd_ban_ban-succ-r": "Alright, I've banned {@user}\n  Reason: {reason}",
+    "$cmd_ban_ban-succ": "Alright, I've banned {@user}",
+    "$cmd_ban_reason-x": "Banned by: {author} ({authorID}) -- No reason specified.",
+    "$cmd_ban_reason": "Banned by: {author} ({authorID}) -- Reason: {reason}",
+    "$cmd_ban_ban-fail": "I was unable to ban {@user}!\nReason: {err}",
 };

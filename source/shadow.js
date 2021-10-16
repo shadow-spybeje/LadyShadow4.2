5ebfff1d147cbae8ab04a6504faf105d53ccd18a
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2021-10-15 17:03:10 -0700

    Fixed Guild/Member join/leave events...
    Removed the `bot` variable.
    
    I thought I already did this; it was causing errors when the bot was running these functions, but for some reason when i ran it manually via eval; the issue did not persist...

--- source/shadow.js
+++ source/shadow.js
@@ -21,10 +21,10 @@ bot.on('ready', (_bot) => {
     else{ Events['READY'](_bot); };
 });
 bot.on('message', (message) => { Events['MESSAGE'](message) }); //New Message
-bot.on('guildCreate', (guild) => { Events['GUILD_JOINED'](bot, guild) }); //JoinsGuild
-bot.on('guildDelete', (guild) => { Events['GUILD_LEFT'](bot, guild) }); //LeavesGuild
-bot.on('guildMemberAdd', (member) => { Events['MEMBER_JOINED'](bot, member) }); //memberJoins
-bot.on('guildMemberRemove', (member) => { Events['MEMBER_LEFT'](bot, member) }); //memberLeaves
+bot.on('guildCreate', (guild) => { Events['GUILD_JOINED'](guild) }); //JoinsGuild
+bot.on('guildDelete', (guild) => { Events['GUILD_LEFT'](guild) }); //LeavesGuild
+bot.on('guildMemberAdd', (member) => { Events['MEMBER_JOINED'](member) }); //memberJoins
+bot.on('guildMemberRemove', (member) => { Events['MEMBER_LEFT'](member) }); //memberLeaves
 
 
 bot.login(require("../../../tokens.json").Beta)
